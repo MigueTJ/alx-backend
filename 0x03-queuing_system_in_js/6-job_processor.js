74310746bbc040b459801170a58bbb749652f310
migueTj <miguejustus@gmail.com>
2024-05-06 07:46:52 +0300

    Create the Job creator

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,14 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+const queueName = 'push_notification_code';
+
+function sendNotification (phoneNumber, message) {
+    console.log(`Sendng notification to ${phoneNumber}, with message: ${message}`);
+}
+
+queue.process(queueName, (job, done) => {
+    const { phoneNumber, message } = job.data;
+    sendNotification(phoneNumber, message);
+    done();
+});
